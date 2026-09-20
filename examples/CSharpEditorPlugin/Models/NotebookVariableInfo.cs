dfbf0589f810a7d225682af56aed95b091e7ca0a
PrashantUnity <priyadarshisadiku@gmail.com>
2026-09-20 09:46:30 +0530

    chore: remove in-tree CSharpEditorPlugin in preparation for git submodule

--- examples/CSharpEditorPlugin/Models/NotebookVariableInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace PdfEditorApp.Plugins.CSharpEditor.Models;
-
-public class NotebookVariableInfo
-{
-    public string Name { get; set; } = string.Empty;
-    public string TypeName { get; set; } = string.Empty;
-    public string ValueDisplay { get; set; } = string.Empty;
-    public string Kind { get; set; } = "Variable";
-}
